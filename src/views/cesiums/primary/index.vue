83fbf34542bd2c0a818514c00d38265c4d788272
cityboy <1334417517@qq.com>
2021-05-09 13:54:17 +0800

    test

--- src/views/cesiums/primary/index.vue
+++ src/views/cesiums/primary/index.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="content">
     <card-list />
+    6666
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent, onMounted } from 'vue';
-  import CardList from '../components/CardList.vue';
+import { defineComponent, onMounted } from 'vue';
+import CardList from '../components/CardList.vue';
 
-  export default defineComponent({
-    components: { CardList },
-    setup() {
-      onMounted(() => {});
+export default defineComponent({
+  components: { CardList },
+  setup() {
+    onMounted(() => {});
 
-      return {};
-    },
-  });
+    return {};
+  },
+});
 </script>
